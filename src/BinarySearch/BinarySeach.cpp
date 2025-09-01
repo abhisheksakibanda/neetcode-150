d22ff3076b728c183886a935788f1a25fd283130
Gyan Karthik Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2025-09-01 19:38:51 -0400

    Add leetcode problem solutions

---
+++ src/BinarySearch/BinarySeach.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <cmath>
+
+#include "../utils/PrettyPrinter.h"
+
+using namespace std;
+
+class Solution {
+public:
+    static int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (target > nums[mid])
+                left = mid + 1;
+            else if (target < nums[mid])
+                right = mid - 1;
+        }
+        return -1;
+    }
+};
+
+int main() {
+    vector testCases = {
+        make_pair<vector<int>, int>({-1, 0, 3, 5, 9, 12}, 9),
+        make_pair<vector<int>, int>({-1, 0, 3, 5, 9, 12}, 2),
+        make_pair<vector<int>, int>({2, 5}, 5)
+    };
+
+    for (int i = 0; i < testCases.size(); ++i) {
+        auto testCase = testCases[i];
+        int result = Solution::search(testCase.first, testCase.second);
+        cout << i + 1 << ": " << PrintElement(testCase) << " -> " << result << endl;
+    }
+
+    return 0;
+}
---
+++ src/Stack/ValidParentheses.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+#include "../utils/PrettyPrinter.h"
+
+using namespace std;
+
+class Solution {
+public:
+    static bool isValid(string s) {
+        stack<char> charStack;
+        for (char& c : s) {
+            if (c == '(') charStack.push(')');
+            else if (c == '{') charStack.push('}');
+            else if (c == '[') charStack.push(']');
+            else {
+                if (charStack.empty() || charStack.top() != c) return false;
+                charStack.pop();
+            }
+        }
+        return charStack.empty();
+    }
+};
+
+int main() {
+    vector<string> testCases = {"()", "()[]{}", "(]", "([])", "([)]"};
+
+    for (int i = 0; i < testCases.size(); ++i) {
+        string& testCase = testCases[i];
+        const bool result = Solution::isValid(testCase);
+        cout << i + 1 << ": " << PrintElement(testCase) << " -> " << (result ? "true" : "false") << endl;
+    }
+
+    return 0;
+}

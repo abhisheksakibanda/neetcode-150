f3c52c8978aae70e904da801334909ce55e8681d
Gyan Karthik Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2025-09-01 20:05:04 -0400

    Add leetcode problem solutions

---
+++ src/SlidingWindow/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../utils/PrettyPrinter.h"
+
+using namespace std;
+
+class Solution {
+public:
+    static int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
+
+int main() {
+    vector<vector<int>> testCases = {{7, 1, 5, 3, 6, 4}, {7, 6, 4, 3, 1}, {1, 2}, {1, 2, 4, 2, 5, 7, 2, 4, 9, 0, 9}};
+
+    for (int i = 0; i < testCases.size(); ++i) {
+        auto testCase = testCases[i];
+        int result = Solution::maxProfit(testCase);
+        cout << i + 1 << ": " << PrintContainer(testCase) << " -> " << result << endl;
+    }
+
+    return 0;
+}

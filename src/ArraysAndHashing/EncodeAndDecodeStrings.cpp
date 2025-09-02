9a55ad6b68818c61b89e81cc3759bb5eea5d9a3d
Gyan Karthik Abhishek Sakibanda <abhisheksakibanda@gmail.com>
2025-09-02 02:43:27 -0400

    Add leetcode problem solutions

---
+++ src/ArraysAndHashing/EncodeAndDecodeStrings.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../utils/PrettyPrinter.h"
+
+using namespace std;
+
+class Solution {
+public:
+    static string encode(vector<string>& strs) {
+        string result;
+
+        for (string& word : strs) {
+            result += to_string(word.size()) + "#" + word;
+        }
+        return result;
+    }
+
+    static vector<string> decode(string s) {
+        vector<string> wordList;
+        size_t i = 0;
+        while (i < s.size()) {
+            int length = 0;
+            while (s[i] != '#') {
+                length = length * 10 + s[i] - '0';
+                i++;
+            }
+            i++;
+            wordList.push_back(s.substr(i, length));
+            i += length;
+        }
+        return wordList;
+    }
+};
+
+int main() {
+    vector<vector<string>> testCases = {{"neet", "code", "love", "you"}, {"we", "say", ":", "yes"}, {"we","say",":","yes","!@#$%^&*()"}};
+
+    for (int i = 0; i < testCases.size(); ++i) {
+        vector<string> testCase = testCases[i];
+        string encodedResult = Solution::encode(testCase);
+        vector<string> decodedResult = Solution::decode(encodedResult);
+        cout << i + 1 << ": " << PrintContainer(testCase) << " -> " << "Encoded: " << encodedResult << "; Decoded: " <<
+            PrintContainer(decodedResult) << endl;
+    }
+
+    return 0;
+}
---
+++ src/BitManipulation/NumberOf1Bits.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+class Solution {
+public:
+    static int hammingWeight(int n) {
+        int hammingWeight = 0;
+
+        while (n) {
+            n &= n - 1;
+            ++hammingWeight;
+        }
+
+        return hammingWeight;
+    }
+};
+
+int main() {
+    vector testCases = {11, 128, 2147483645};
+
+    for (int i = 0; i < testCases.size(); ++i) {
+        auto testCase = testCases[i];
+        int result = Solution::hammingWeight(testCase);
+        cout << i + 1 << ": " << testCase << " -> " << result << endl;
+    }
+
+    return 0;
+}
